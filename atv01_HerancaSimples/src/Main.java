721fcbe0c6d1f5f766cf9748ff2b9467e3e47cbd
emile <emilepaula53@gmail.com>
2026-04-08 17:43:17 -0300

    Herança simples

---
+++ atv01_HerancaSimples/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] arg){
+        Professor professor = new Professor("Lula", 75, "Geografia");
+
+        professor.aprensentar();
+        professor.ensinar();
+
+
+    }
+}
---
+++ atv01_HerancaSimples/src/Professor.java
@@ -0,0 +1,21 @@
+public class Professor extends Pessoa {
+    private String disciplina;
+
+    public Professor(String nome, int idade, String disciplina) {
+        super(nome, idade);
+        this.disciplina = disciplina;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public void ensinar() {
+        System.out.println("Disciplina: " + disciplina);
+    }
+
+}
